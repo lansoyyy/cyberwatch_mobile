0289f9ce831eeb65bb8bf927f53121c9412b5198
Lans12345 <olanalans12345@gmail.com>
2023-07-02 18:58:09 +0800

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
-import 'package:cyberwatch_mobile/screens/scan_screen.dart';
+import 'package:cyberwatch_mobile/screens/auth/landing_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: 'cyberwatch-dfd11',
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -11,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: ScanScreen(),
+      home: LandingScreen(),
     );
   }
 }
