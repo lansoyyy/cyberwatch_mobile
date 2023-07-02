22dbf357937c94e718fe956063d7cb04666aba61
Lans12345 <olanalans12345@gmail.com>
2023-07-03 05:52:45 +0800

    added biometrics

--- lib/screens/scan_screen.dart
+++ lib/screens/scan_screen.dart
@@ -1,8 +1,67 @@
 import 'package:cyberwatch_mobile/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:local_auth/local_auth.dart';
 
-class ScanScreen extends StatelessWidget {
+class ScanScreen extends StatefulWidget {
   const ScanScreen({super.key});
+
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  String _authorized = 'Not Authorized';
+  bool _isAuthenticating = false;
+  bool authenticated = false;
+
+  Future<void> _authenticateWithBiometrics() async {
+    // only Biometrics
+
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Authenticating';
+      });
+      authenticated = await auth.authenticate(
+        localizedReason:
+            'Scan your fingerprint (or face or whatever) to authenticate',
+        options: const AuthenticationOptions(
+          stickyAuth: true,
+          biometricOnly: true,
+        ),
+      );
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Authenticating';
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Error - ${e.message}';
+      });
+      return;
+    }
+    if (!mounted) {
+      return;
+    }
+
+    final String message = authenticated ? 'Authorized' : 'Not Authorized';
+    setState(() {
+      _authorized = message;
+    });
+
+    if (authenticated) {
+      Fluttertoast.showToast(msg: 'Fingerprint scanned succesfully!');
+    } else {
+      Fluttertoast.showToast(msg: 'Fingerprint scanned unsuccesfully!');
+    }
+    print(authenticated);
+  }
 
   @override
   Widget build(BuildContext context) {
